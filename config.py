b0159a2c166689264a7688b1d67fa9f9d653e8f6
arnob <farhanasfar21@gmail.com>
2026-02-02 00:46:58 +0600

    Set track_modifications to false

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 # a configuration container. to populate app.config dict.
 class Config:
+    # the key 'SQLALCHEMY_DATABASE_URI' has to be exactly like this. Flask expects as it is.
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-
-# the key 'SQLALCHEMY_DATABASE_URI' has to be exactly like this. Flask expects as it is.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
