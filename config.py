32da55a2315348bd4884fb8a579c25022ac85a28
arnob <farhanasfar21@gmail.com>
2026-02-02 00:44:10 +0600

    Add comments for my understanding

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
+# a configuration container. to populate app.config dict.
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+# the key 'SQLALCHEMY_DATABASE_URI' has to be exactly like this. Flask expects as it is.
