78c8ec7c051cc3f0d616c8ce598935c5f456eb5a
arnob <farhanasfar21@gmail.com>
2026-02-02 07:39:01 +0600

    Update route to call services

--- task_manager/routes/tasks_routes.py
+++ task_manager/routes/tasks_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
 from task_manager.extensions import db
-from task_manager.models.tasks_models import Task
+from task_manager.services.tasks_services import create_task
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/api/v1/tasks")
