92968d2343dd7b280d0cd24b91a9533d57392249
farhan <farhanasfar21@gmail.com>
2026-02-02 11:52:25 +0600

    Fix function name: 'delete_task'

--- task_manager/routes/tasks_routes.py
+++ task_manager/routes/tasks_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from task_manager.extensions import db
-from task_manager.services.tasks_services import create_task, get_tasks, get_task_by_id, edit_task, delete_task,  
+from task_manager.services.tasks_services import create_task, get_tasks, get_task_by_id, edit_task, delete_task
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/api/v1/tasks")
 
