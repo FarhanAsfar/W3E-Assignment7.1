9a142e2914639c2ca201f0c14d2994373a99f007
farhan <farhanasfar21@gmail.com>
2026-02-02 15:56:16 +0600

    Fix page import

--- task_manager/routes/pages_routes.py
+++ task_manager/routes/pages_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect
 from task_manager.services.tasks_services import get_tasks, edit_task
 from task_manager.models.tasks_models import TaskStatus
+from task_manager.services.tasks_services import get_task_by_id
 
 pages_bp = Blueprint("pages", __name__)
 
@@ -24,7 +25,18 @@ def tasks_page():
     )
 
 
-@pages_bp.route("/tasks/<int:task_id>/mark-done")
-def mark_task_done(task_id):
-    edit_task(task_id, {"status": TaskStatus.DONE.value})
+@pages_bp.route("/tasks/<int:task_id>/toggle-status")
+def toggle_task_status(task_id):
+    task = get_task_by_id(task_id)
+
+    if not task:
+        return redirect("/tasks")
+
+    new_status = (
+        TaskStatus.TODO.value
+        if task.status == TaskStatus.DONE
+        else TaskStatus.DONE.value
+    )
+
+    edit_task(task_id, {"status": new_status})
     return redirect("/tasks")
