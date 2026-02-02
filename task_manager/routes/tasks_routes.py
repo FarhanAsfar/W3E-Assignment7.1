81aff43884b4cddcc9f0dbb72bc96d5dd266a8a7
arnob <farhanasfar21@gmail.com>
2026-02-02 07:47:11 +0600

    Add post route to create tasks

--- task_manager/routes/tasks_routes.py
+++ task_manager/routes/tasks_routes.py
@@ -3,3 +3,19 @@ from task_manager.extensions import db
 from task_manager.services.tasks_services import create_task
 
 task_bp = Blueprint("tasks", __name__, url_prefix="/api/v1/tasks")
+
+@task_bp.route("", methods=["POST"])
+def create_task_route():
+    try:
+        task = create_task(request.get_json())
+
+        return jsonify({
+            "id": task.id,
+            "title": task.title,
+            "status": task.status,
+            "due_date": task.due_date,
+            "created_at": task.created_at
+        }), 201
+
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
