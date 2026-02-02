b54a0f35424283a6de99de0e8e3819039d7a65c2
arnob <farhanasfar21@gmail.com>
2026-02-02 07:53:54 +0600

    Update Task model: add Enum to status column

--- task_manager/models/tasks_models.py
+++ task_manager/models/tasks_models.py
@@ -1,14 +1,18 @@
 from task_manager.extensions import db
 from sqlalchemy.sql import func
+import enum
 
-
+class TaskStatus(enum.Enum):
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
 class Task(db.Model):
     __tablename__ = "tasks"
 
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(), nullable = False)
     description = db.Column(db.Text, nullable = True)
-    status = db.Column(db.Boolean, default = False)
+    status = db.Column(db.Enum(TaskStatus), nullable=False, default = TaskStatus.TODO)
     
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     due_date = db.Column(db.Date, nullable = True)
