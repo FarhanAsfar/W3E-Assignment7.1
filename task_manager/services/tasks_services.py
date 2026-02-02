b6050bc43276786c0ff6b044e6d05583bf7e721e
arnob <farhanasfar21@gmail.com>
2026-02-02 07:58:56 +0600

    Update create_task func in servie to add status enum

--- task_manager/services/tasks_services.py
+++ task_manager/services/tasks_services.py
@@ -1,18 +1,29 @@
 from task_manager.extensions import db
-from task_manager.models.tasks_models import Task 
+from task_manager.models.tasks_models import Task, TaskStatus 
 from datetime import date 
 
+# create task from given user request
 def create_task(data):
     title = data.get("title")
     description = data.get("description")
     due_date = data.get("due_date")
+    status = data.get("status", TaskStatus.TODO.value)
 
     if not title:
         raise ValueError("Title is required")
+    
+    # validate status
+    try:
+        status_enum = TaskStatus(status)
+    except ValueError:
+        raise ValueError(
+            f"Invalid status. Allowed values: {[s.value for s in TaskStatus]}"
+        )
 
     task = Task(
         title=title,
         description=description,
+        status=status_enum,
         due_date=date.fromisoformat(due_date) if due_date else None
     )
 
